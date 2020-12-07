8289457ac269269909581cf505eb633e35ae5205
Colin Fraser <colin.r.fraser@gmail.com>
2020-12-07 13:33:01 -0800

    part 2

--- R/1.R
+++ R/1.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+library(httr)
+
+# get data ----------------------------------------------------------------
 
 session_cookie <- rstudioapi::askForSecret('aoc_session')
 data <- httr::GET("https://adventofcode.com/2020/day/1/input",
@@ -7,7 +10,18 @@ data <- httr::GET("https://adventofcode.com/2020/day/1/input",
   read_lines() %>%
   as.numeric()
 
+# Part 1 ------------------------------------------------------------------
+
 crossing(c1 = data, c2 = data) %>%
   filter(c1 <= c2) %>%
-  filter(c1 + c2 == 2000) %>%
+  filter(c1 + c2 == 2020) %>%
   summarise(solution = c1 * c2)
+
+
+# Part 2 ------------------------------------------------------------------
+
+crossing(c1 = data, c2 = data, c3 = data) %>%
+  filter(c1 <= c2, c2 <= c3) %>%
+  filter(c1 + c2 + c3 == 2020) %>%
+  summarise(solution = c1 * c2 * c3)
+
