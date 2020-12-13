7d5d71570c0ee829fc36e929d54eff5285b46ba8
colin-fraser <colin.r.fraser@gmail.com>
2020-12-12 19:42:11 -0800

    fix problem 3

--- R/3.R
+++ R/3.R
@@ -1,4 +1,4 @@
-source('R/helpers.R')
+source("R/helpers.R")
 library(stringr)
 library(purrr)
 
@@ -7,5 +7,22 @@ input_data <- aoc_get_data(3)
 # Problem 1 -----------------------------------------------------------------------------------
 
 n <- nchar(input_data[1])
-tree_locations <- str_locate_all(input_data, '#')
-tree_locations %>%
+# for indices y = 1..n and tree locations x, you hit a tree when
+# 3(y-1)+1 = x mod n
+input_data %>%
+  str_locate_all("#") %>%
+  map(~ .x[, 1]) %>%
+  imap_lgl(~ any((3 * (.y - 1) + 1 - .x) %% n == 0)) %>%
+  sum()
+
+# Problem 2 -----------------------------------------------------------------------------------
+f <- function(slope, idata = input_data) {
+  n <- nchar(idata)
+  idata %>%
+    str_locate_all("#") %>%
+    map(~ .x[, 1]) %>%
+    imap_lgl(~ any((slope * (.y - 1) + 1 - .x) %% n == 0)) %>%
+    sum()
+}
+
+prod(sapply(c(1, 3, 5, 7, .5), f))
